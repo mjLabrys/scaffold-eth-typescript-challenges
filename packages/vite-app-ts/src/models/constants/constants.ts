acf0784c569e19deec29d8cde4cb75d6a2e7d8ef
mjLabrys <mj@labrys.io>
2022-04-12 12:24:25 +1000

    Updated Infura ID

--- packages/vite-app-ts/src/models/constants/constants.ts
+++ packages/vite-app-ts/src/models/constants/constants.ts
@@ -1,5 +1,5 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = '3ccea23c26224daf802cf31e4699d15e';
+export const INFURA_ID = '207d5cc2a60f48769b7a195e6702f120';
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
 export const ETHERSCAN_KEY = 'PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8';
